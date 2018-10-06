1bb98db274b1afdd2861841b583ac14c1aa9ca2b
puneetsawhney000 <43901011+puneetsawhney000@users.noreply.github.com>
2018-10-06 11:20:35 +0530

    Add files via upload

---
+++ GRP.C
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+void main()
+{
+int i;
+clrscr();
+//window(1000,10,100,100);
+textcolor(WHITE);
+textbackground(BLUE);
+clrscr();
+cprintf("\r\nษอออ[þ]ออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออป");
+cprintf("\r\nบ                                 WELCOME                                    ฑ");
+cprintf("\r\nบ                            TO KBC QUIZ GAME                                ฑ");
+cprintf("\r\nบ                                                                            ฑ");
+cprintf("\r\nบ          $**** Welcome To Kaun Banega Karodpati ****$                      ฑ");
+cprintf("\r\nบ                         ARE YOU READY TO PLAY ........?                    ฑ");
+cprintf("\r\nบ                                 RULES for KBC                              ฑ");
+cprintf("\r\nบ         1.The game consists of 10 questions.                               ฑ");
+cprintf("\r\nบ         2.Every question consists of 4 options                             ฑ");
+cprintf("\r\nบ                  there will be only one correct answer.                    ฑ");
+cprintf("\r\nบ         3.For every correct answer you will be getting 5000 rs of money    ฑ");
+cprintf("\r\nบ         4.You will be exit out of game for wrong answer.                   ฑ");
+cprintf("\r\nบ         5.You can use 50-50 option only one time.                          ฑ");
+cprintf("\r\nบ         6.50-50 option is avilable only after 8th question.                ฑ");
+cprintf("\r\nบ                                                                            ฑ");
+cprintf("\r\nบ        If you are intrested to play game press ENTER to enter into game.   ฑ");
+cprintf("\r\nบ                                                                            ฑ");
+cprintf("\r\nบ                                                                            ฑ");
+cprintf("\r\nศออฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐฐอผ");
+
+getch();
+
+   /*
+for(i=0;i<255;i++)
+{
+  printf("\t %d=%c",i,i);
+}
+
+getch();
+   */
+
+
+}
